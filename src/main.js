f477d14dfd719982855fb823dcef668f0b3e680b
DennisChiira <programmingchiira@gmail.com>
2024-02-08 11:34:42 +0300

    commit

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
+import './registerServiceWorker'
 
 const app = createApp(App);
 
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
+const { GenerateSW } = require("workbox-webpack-plugin");
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
